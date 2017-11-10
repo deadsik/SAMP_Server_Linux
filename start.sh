053fb12aa534589d5cfb5f3283a8b68177350d81
deadsik <deadsik@ukr.net>
2017-11-10 06:06:32 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 for i in `screen -ls |grep 'samp' |awk '{print $1}'` ;  do screen -X -S $i quit ; done
-/root/samp03/start.sh
+screen -A -m -d -S samp sudo -H -u server bash -c 'cd /home/server/samp03  && /home/server/samp03/samp03svr'
