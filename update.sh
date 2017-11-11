6bfdf756547c0421a3b3177c81be91b98cd3d01e
deadsik <deadsik@ukr.net>
2017-11-11 05:25:23 +0200

    Update update.sh

--- update.sh
+++ update.sh
@@ -4,3 +4,5 @@ HOSTNAME="DARK-GAMES.ORG.UA"
 FILE="/home/server/samp03/server.cfg"
 sed -i 's/.*rcon_password.*/rcon_password '$PASSWORD'/' $FILE
 sed -i 's/.*hostname.*/hostname '$HOSTNAME'/' $FILE
+sed -i '/update.sh/d' /root/restart_all.sh
+rm -f /root/update.sh
