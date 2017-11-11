aec8d200a3b60a67cf72d70de48216efa7ada3d8
deadsik <deadsik@ukr.net>
2017-11-11 05:21:53 +0200

    Create update.sh

---
+++ update.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+PASSWORD=`< /dev/urandom tr -dc A-Z-a-z-0-9 | head -c6`
+HOSTNAME="DARK-GAMES.ORG.UA"
+FILE="/home/server/samp03/server.cfg"
+sed -i 's/.*rcon_password.*/rcon_password '$PASSWORD'/' $FILE
+sed -i 's/.*hostname.*/hostname '$HOSTNAME'/' $FILE
